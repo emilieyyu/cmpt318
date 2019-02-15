a89831f588ae03797b184ade2fe904a79709c764
emilieyyu <emilieyyu@hotmail.com>
2019-02-14 17:00:37 -0800

    travel.txt data analysis

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# cmpt318
-textual analysis project
+# CMPT318 Textual Analysis Project
+Analyzing text files using various techniques and extracting useful information based on investigation. Our main goals will be to compare *themes, content words, author's diction, structure/style, targeted audience*.
+
+### Corpus
+- time machine
+- travel
+- xab
+- 4th files
+
+### Cleaning Up
+######time machine
+######travel
+Manually remove errors and fix typos 'l;and' and 'manyof'  
+######xab  
+######4th file  
+
+### Data Analysis
+Analyze by extracting useful information using:  
+- tokens  
+- types/frequency  
+- ngrams  
+- content words
+
+######time machine
+
+######travel
+Script Execution *travelscript.sh*:  
+1. ```chmod +x travelscript.sh```  
+2. ```./travelscript.sh```  
+What the script does:  
+The script removes all extra tabs in front of each line. It then tokenizes the textfile and sorts based on alphabetical order, frequency, and also creates corresponding 2, 3, 4 gram files. *ngram_files* contains all temporary files created during the process for reference.
+
+######xab
+
+######4th file
---
+++ travel/travelscript.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#manually corrected typos - only 2 'l;and' and 'manyof'
+#remove all extra tabs in front of each line
+echo 'Removing tabs in front of lines...'
+sed 's/^ *//g' < travel.txt > travel_clean.txt
+#tokenization
+echo 'Tokenizing...'
+cat travel_clean.txt | tr -cs 'a-zA-Z0-9' '[\n*]' | tr 'A-Z' 'a-z' > travel_tokens.txt
+#alphabetically organized
+echo 'Sorting alphabetically...'
+cat travel_clean.txt | tr -cs 'a-zA-Z0-9' '[\012*]' | tr A-Z a-z | sort | uniq > travel_alph.txt
+#sorted based on frequency
+echo 'Sorting based on frequency...'
+cat travel_tokens.txt |  sort | uniq -c | sort -rn > travel_types.txt
+#2, 3, 4 grams with frequency
+echo 'Extracting 2-gram count...'
+cat travel_clean.txt | tr -cs [a-zA-Z0-9] '[\012*]' | tr A-Z a-z > tmpfile1.txt
+tail +2 tmpfile1.txt > tmpfile2.txt
+paste tmpfile1.txt tmpfile2.txt | sort | uniq -c | sort -rn > 2-grams.txt
+echo 'Extracting 3-gram count...'
+paste tmpfile1.txt tmpfile2.txt > tmpfile3.txt
+tail +3 tmpfile1.txt > tmpfile4.txt
+paste tmpfile3.txt tmpfile4.txt | sort | uniq -c | sort -rn > 3-grams.txt
+echo 'Extracting 4-gram count...'
+tail +4 tmpfile1.txt > tmpfile5.txt
+paste tmpfile3.txt tmpfile4.txt > tmpfile6.txt
+paste tmpfile6.txt tmpfile5.txt | sort | uniq -c | sort -rn > 4-grams.txt
+mkdir ngram_files
+mv tmpfile1.txt tmpfile2.txt tmpfile3.txt tmpfile4.txt tmpfile5.txt tmpfile6.txt ngram_files
+echo 'Complete.'
