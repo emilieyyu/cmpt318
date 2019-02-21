77c345128b36d7ff96b3fbb7dc2fa11beb152dad
Song Xiao <sxa17@sfu.ca>
2019-02-20 21:59:52 -0800

    Divide xab into 9 parts, each part is an independent paper/thesis. Clean up each part of text and run 'xab_part_script.sh' on each part to perform text analysis. The results are stored in corresponding folder for each part.

---
+++ Xab/xab_clean&results/xab_part_script.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+## Create a folder for each part of xab
+new_dir="`basename $1 .txt`_results"
+mkdir -p $new_dir
+
+## Tokenize text and replace de-capitalize all characters
+# Note: single dash '-', usually used as part of one word (i.e. ACT-R), is kept.
+echo "Tokenizing..."
+cat $* | tr -cs 'a-zA-Z0-9/-' '[\012*]' | tr A-Z a-z > tokens.txt
+
+## Extract all types and sort them by frequency
+echo "Extracting types..."
+cat tokens.txt | sort | uniq -c | sort -rn > types.txt
+
+## Generate n-gram results where n = 2,3,4
+cp tokens.txt tmpfile1
+echo "Extracting 2-gram count..."
+tail -n +2 tmpfile1 > tmpfile2
+paste tmpfile1 tmpfile2 | sort | uniq -c | sort -rn > 2-grams.txt
+echo "Extracting 3-gram count..."
+paste tmpfile1 tmpfile2 > tmpfile3
+tail -n +3 tmpfile1 > tmpfile4
+paste tmpfile3 tmpfile4 | sort | uniq -c | sort -rn > 3-grams.txt
+echo "Extracting 4-gram count..."
+tail -n +4 tmpfile1 > tmpfile5
+paste tmpfile3 tmpfile4 > tmpfile6
+paste tmpfile6 tmpfile5 | sort | uniq -c | sort -rn > 4-grams.txt
+rm -f tmpfile1 tmpfile2 tmpfile3 tmpfile4 tmpfile5 tmpfile6
+
+## Calculate average words per sentence (see xab clean details for unsolved problems)
+# version 1:
+# cat $* | tr '\r\n' ' ' | tr '.?!' '[\012*]' > sentences.txt
+
+## Identify content words: 
+# Tag the text with "Lapos" -> tokenize -> extract types and sort -> grep nouns.
+# After tagging, some words are followed by punctuation. Thus all punctuations except dash '-' are removed before sorting.
+echo "Tagging text..."
+./tagger_v3.sh < $* > tagged_text.txt
+cat tagged_text.txt | tr -s '[:blank:]' '[\012*]' | tr -d '.,:“”();[]' | tr A-Z a-z  > tagged_tokens.txt
+cat tagged_tokens.txt | sort | uniq -c | sort -rn > tagged_types.txt
+grep '/nn' tagged_types.txt > nouns.txt
+
+echo "Move results to directory: $new_dir"
+mv tokens.txt types.txt 2-grams.txt 3-grams.txt 4-grams.txt $new_dir
+mv tagged_text.txt tagged_tokens.txt tagged_types.txt nouns.txt $new_dir
+
+
